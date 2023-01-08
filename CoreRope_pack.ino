8b869c9b2cfedb1e515d08c177ad98d06c99fe5f
Rhein <112055313+coprolitebbs@users.noreply.github.com>
2023-01-08 16:14:33 +0300

    Update CoreRope_pack.ino
    Поправил комментарии

--- CoreRope_pack.ino
+++ CoreRope_pack.ino
@@ -1,4 +1,3 @@
-
 //Адреса пинов снимающих сигнал с ферритовых колец
 uint8_t in_pins[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
@@ -10,41 +9,46 @@ int clockPin = 13;
 //Пин подключен к DS входу 74HC595
 int dataPin = 11;
 
-
-//Вывод на 16битный сдвиговый регистр числа, кодирующего пины
+//Вывод на 16-битный сдвиговый регистр числа, кодирующего пины
 void shift_out_value(unsigned int val) {
   digitalWrite(latchPin, LOW);
   //Выведем на сдвиговый регистр последний байт
   shiftOut(dataPin, clockPin, MSBFIRST, highByte(val));
-  //Выведем на сдвиговый регистр последний байт
+  //Выведем на сдвиговый регистр первый байт
   shiftOut(dataPin, clockPin, MSBFIRST, lowByte(val));
   digitalWrite(latchPin, HIGH);
 }
 
-
 //Разрядка конденсаторов на датчиках колец
 void discharge() {
   for (int i = 0; i < 8; i++) {
+    //Переключить пин на выход
     pinMode(in_pins[i], OUTPUT);
+    //занулить
     digitalWrite(in_pins[i], LOW);
+    //небольшая задержка
     delay(3);
+    //Переключить обратно на вход
     pinMode(in_pins[i], INPUT);
   }
 }
 
-
-//Считывание ниббла из 8 бит
-uint8_t read_nibble8(unsigned int address) {
+//Считывание байта с колец
+uint8_t read_ferrite_byte(unsigned int address) {
   uint8_t values[] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-  //Для точности можно увеличить количество проходов чтения, но, работает и так
+  //Для точности можно увеличить количество проходов чтения, но, 
+  //работает и так
   //for (uint8_t phase = 0; phase < 2; phase++)
   {
     //Разрядим
     discharge();
-    //Подергаем нужным проводком по нужному адресу через сдвиговый регистр
-    //Это нужно чтобы создать в ферритовых кольцах магнитный поток и зарядить конденсаторы
-    //на датчиках тех колец, через которые проходит проводник с нужным адресом
+    //Подергаем нужным проводком по нужному адресу через сдвиговый 
+    //регистр
+    //Это нужно чтобы создать в ферритовых кольцах магнитный поток и 
+    //зарядить конденсаторы
+    //на датчиках тех колец, через которые проходит проводник с нужным 
+    //адресом
     for (uint8_t j = 0; j < 8; j++) {
       shift_out_value(address);
       shift_out_value(0);
@@ -58,19 +62,15 @@ uint8_t read_nibble8(unsigned int address) {
 
   discharge();
 
-  //Упакуем все считанные значения в один байт, где 0 - не считано ничего, 1 - считано что-то
+  //Упакуем все считанные значения в один байт, где 0 - не считано 
+  //ничего, 1 - считано что-то
   uint8_t result = 0;
   for (uint8_t ix = 0; ix < 8; ix++)
   {
     result = (result << 1) | ((values[ix] > 1) ? 1 : 0);
-    //Serial.print(values[ix], DEC);
-    //Serial.print(" ");
   }
-  //Serial.println();
   return result;
 }
-
-
 
 void setup() {
   pinMode(latchPin, OUTPUT);
@@ -82,7 +82,7 @@ void setup() {
   }
 
   Serial.begin(115200);
-
+  
   //разрядим конденсаторы
   discharge();
   Serial.println("------------------------------");
@@ -96,31 +96,23 @@ void setup() {
   Serial.println();
   Serial.println();
 
-  //Считаем и выведем нибблы по адресам 1..16
+  //Считаем и выведем значения по адресам 1..16
   for (int i = 0; i < 16; i++) {
     Serial.print("address[");
     if (i < 10) Serial.print("0");
     Serial.print( i, DEC);
     Serial.print("] = ");
-    uint8_t nibble = read_nibble8((unsigned int)1 << i);
-    Serial.print(nibble, DEC);
+    uint8_t nibbles = read_ferrite_byte((unsigned int)1 << i);
+    Serial.print(nibbles, DEC);
     Serial.print(" - ");
-    Serial.print(nibble, BIN);
-    if (nibble > 32) {
+    Serial.print(nibbles, BIN);
+    if (nibbles > 32) {
       Serial.print(" - ");
-      Serial.print((char)nibble);
+      Serial.print((char)nibbles);
     }
     Serial.println();
   }
 }
 
-
-
-
-
-
 void loop() {
-
-
-
 }
